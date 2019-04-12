687cb4a2d1cd39b2438ba01fb09be311da381f58
Victor Kimosop Chepkoit <VCHEPKOIT@safaricom.co.ke>
2019-04-12 13:24:57 +0300

    randomize the dice

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,17 @@ var playerOne = new Player("Player One");
 var playerTwo = new Player("Player Two");
 
 
+// Dice Constructor (dice sides = 6)
+function Dice(sides) {
+  this.sides = sides || 6;
+}
+
+// Roll Prototype - Gets a random number between 1 and 6
+Dice.prototype.roll = function() {
+  var roll = Math.floor((Math.random() * this.sides ) + 1);
+  if (roll === 1) {
+    alert("Pole ! You rolled a 1. Your turn is over.");
+  }
+  return roll;
+}
+
